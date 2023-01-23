cc1217d9a06af93ef7f936b0ed4d94a2ea6a8afd
poornachandu28 <110621498+poornachandu28@users.noreply.github.com>
2023-01-23 11:45:02 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,23 @@
-print("Hello world")
+name: Run Script
+
+on:
+  push:
+    branches: [ master ]
+  pull_request:
+    branches: [ master ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
+        with:
+          python-version: '3.x'
+      - uses: jannekem/run-python-script-action@v1
+        with:
+          script: |
+            import os
+            print("Hello world")
+            for f in os.listdir():
+                print(f)
